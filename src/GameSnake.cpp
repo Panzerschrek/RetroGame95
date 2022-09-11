d4f79c17366b50e32c9c74aeeef0887b27395f4f
Panzerschrek <AntiPanzerschrek@yandex.ru>
2022-09-11 21:38:58 +0300

    Realizovano bazovoje upravlenije zmejoj.

--- src/GameSnake.cpp
+++ src/GameSnake.cpp
@@ -8,6 +8,7 @@ GameSnake::GameSnake(SoundPlayer& sound_player)
 	: sound_player_(sound_player)
 	, rand_(Rand::CreateWithRandomSeed())
 {
+	SpawnSnake();
 }
 
 void GameSnake::Tick(const std::vector<SDL_Event>& events, const std::vector<bool>& keyboard_state)
@@ -20,6 +21,37 @@ void GameSnake::Tick(const std::vector<SDL_Event>& events, const std::vector<boo
 		{
 			next_game_ = std::make_unique<GameMainMenu>(sound_player_);
 		}
+		if(event.type == SDL_KEYDOWN && snake_ != std::nullopt)
+		{
+			// Turn snake, but do not allow to turn directly towards the neck.
+			if(event.key.keysym.scancode == SDL_SCANCODE_LEFT &&
+				snake_->segments[0].position[0] <= snake_->segments[1].position[0])
+			{
+				snake_->direction = SnakeDirection::XMinus;
+			}
+			if(event.key.keysym.scancode == SDL_SCANCODE_RIGHT &&
+				snake_->segments[0].position[0] >= snake_->segments[1].position[0])
+			{
+				snake_->direction = SnakeDirection::XPlus;
+			}
+			if(event.key.keysym.scancode == SDL_SCANCODE_DOWN &&
+				snake_->segments[0].position[1] >= snake_->segments[1].position[1])
+			{
+				snake_->direction = SnakeDirection::YPlus;
+			}
+			if(event.key.keysym.scancode == SDL_SCANCODE_UP &&
+				snake_->segments[0].position[1] <= snake_->segments[1].position[1])
+			{
+				snake_->direction = SnakeDirection::YMinus;
+			}
+		}
+	}
+
+	++num_ticks_;
+
+	if(num_ticks_ % 60 == 0)
+	{
+		MoveSnake();
 	}
 }
 
@@ -27,41 +59,86 @@ void GameSnake::Draw(const FrameBuffer frame_buffer)
 {
 	FillWholeFrameBuffer(frame_buffer, g_color_black);
 
-	uint32_t offset_y = 16;
-	uint32_t offset_x = 16;
+	const uint32_t field_offset_x = 10;
+	const uint32_t field_offset_y = 10;
 
-	const SpriteBMP tail_sprite(Sprites::snake_tail);
-	DrawSpriteWithAlphaUnchecked(
-		frame_buffer,
-		tail_sprite,
-		0,
-		offset_x,
-		offset_y);
-	offset_y += tail_sprite.GetHeight();
-
-	const SpriteBMP body_segment_sprite(Sprites::snake_body_segment);
-
-	for(uint32_t i = 0; i < 10; ++i)
+	if(snake_ != std::nullopt)
 	{
-		DrawSpriteWithAlphaUnchecked(
-			frame_buffer,
-			body_segment_sprite,
-			0,
-			offset_x,
-			offset_y);
-		offset_y += body_segment_sprite.GetHeight();
-	}
+		const SpriteBMP head_sprite(Sprites::snake_head);
+		const SpriteBMP body_segment_sprite(Sprites::snake_body_segment);
+		const SpriteBMP tail_sprite(Sprites::snake_tail);
 
-	const SpriteBMP head_sprite(Sprites::snake_head);
-	DrawSpriteWithAlphaUnchecked(
-		frame_buffer,
-		head_sprite,
-		0,
-		offset_x,
-		offset_y);
+		for(const SnakeSegment& segment : snake_->segments)
+		{
+			SpriteBMP sprite = body_segment_sprite;
+			if(&segment == &snake_->segments.front())
+			{
+				sprite = head_sprite;
+			}
+			if(&segment == &snake_->segments.back())
+			{
+				sprite = tail_sprite;
+			}
+
+			DrawSpriteWithAlphaUnchecked(
+				frame_buffer,
+				sprite,
+				0,
+				field_offset_x + segment.position[0] * c_block_size,
+				field_offset_y + segment.position[1] * c_block_size);
+
+		}
+	}
 }
 
 GameInterfacePtr GameSnake::AskForNextGameTransition()
 {
 	return std::move(next_game_);
+}
+
+void GameSnake::SpawnSnake()
+{
+	Snake snake;
+	snake.direction = SnakeDirection::YPlus;
+
+	for(uint32_t i = 0; i < 5; ++i)
+	{
+		SnakeSegment segment;
+		segment.position[0] = c_field_width / 2;
+		segment.position[1] = c_field_height / 2 - i;
+		snake.segments.push_back(segment);
+	}
+
+	snake_ = std::move(snake);
+}
+
+void GameSnake::MoveSnake()
+{
+	if(snake_ == std::nullopt)
+	{
+		return;
+	}
+
+	SnakeSegment new_segment = snake_->segments.front();
+	// TODO - check for walls collision here.
+	switch(snake_->direction)
+	{
+	case SnakeDirection::XPlus:
+		new_segment.position[0] += 1;
+		break;
+	case SnakeDirection::XMinus:
+		new_segment.position[0] -= 1;
+		break;
+	case SnakeDirection::YPlus:
+		new_segment.position[1] += 1;
+		break;
+	case SnakeDirection::YMinus:
+		new_segment.position[1] -= 1;
+		break;
+	}
+
+	snake_->segments.insert(snake_->segments.begin(), new_segment);
+	snake_->segments.pop_back();
+
+	// TODO - check for obstacles collision, self-intersection, food consumption.
 }
--- src/GameSnake.hpp
+++ src/GameSnake.hpp
@@ -2,7 +2,9 @@
 #include "GameInterface.hpp"
 #include "Rand.hpp"
 #include "SoundPlayer.hpp"
-
+#include <array>
+#include <optional>
+#include <vector>
 
 class GameSnake final : public GameInterface
 {
@@ -19,8 +21,41 @@ public: // GameInterface
 	virtual GameInterfacePtr AskForNextGameTransition() override;
 
 private:
+	struct SnakeSegment
+	{
+		std::array<uint32_t, 2> position{};
+	};
+
+	enum class SnakeDirection
+	{
+		XPlus,
+		XMinus,
+		YPlus,
+		YMinus,
+	};
+
+	struct Snake
+	{
+		// Head segment has index 0.
+		std::vector<SnakeSegment> segments;
+		SnakeDirection direction = SnakeDirection::XPlus;
+	};
+
+	static const constexpr uint32_t c_field_width = 20;
+	static const constexpr uint32_t c_field_height = 20;
+	static const constexpr uint32_t c_block_size = 10;
+
+private:
+	void SpawnSnake();
+	void MoveSnake();
+
+private:
 	SoundPlayer& sound_player_;
 	Rand rand_;
+
+	uint32_t num_ticks_ = 0;
+
+	std::optional<Snake> snake_;
 
 	GameInterfacePtr next_game_;
 };
