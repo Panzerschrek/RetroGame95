b731007de69ead99567e15fbb7bfcc122db7cb9a
Panzerschrek <AntiPanzerschrek@yandex.ru>
2022-09-24 16:09:54 +0300

    Sdelana kakaja-to lokalizaçija suscestvujuschih tekstov.

--- src/StringsDe.cpp
+++ src/StringsDe.cpp
@@ -3,40 +3,40 @@
 namespace Strings
 {
 
-// TODO - translate this.
+// TODO - check these translations.
 
 #define DEFINE_STRING(x, y) const char x[] = y;
 
-DEFINE_STRING(main_menu_game_title, "RetroGame95")
-DEFINE_STRING(main_menu_new_game, "New game")
-DEFINE_STRING(main_menu_continue_game, "Continue game")
-DEFINE_STRING(main_menu_select_game, "Select game")
-DEFINE_STRING(main_menu_quit, "Quit")
+DEFINE_STRING(main_menu_game_title, "AltesSpiel95")
+DEFINE_STRING(main_menu_new_game, "Neues Spiel")
+DEFINE_STRING(main_menu_continue_game, "weiter spielen")
+DEFINE_STRING(main_menu_select_game, "Spiel auswählen")
+DEFINE_STRING(main_menu_quit, "Beenden")
 
-DEFINE_STRING(game_name_arkanoid, "Arkanoid")
-DEFINE_STRING(game_name_tetris, "Tetris")
-DEFINE_STRING(game_name_snake, "Snake")
-DEFINE_STRING(game_name_pacman, "Pacman")
+DEFINE_STRING(game_name_arkanoid, "das Verlorenes Raumschiff")
+DEFINE_STRING(game_name_tetris, "der Maurer")
+DEFINE_STRING(game_name_snake, "die Gefreßige Schlange")
+DEFINE_STRING(game_name_pacman, "Packmann")
 
-DEFINE_STRING(arkanoid_round, "Round")
-DEFINE_STRING(arkanoid_score, "Score")
-DEFINE_STRING(arkanoid_game_over, "Game Over")
+DEFINE_STRING(arkanoid_round, "Runde")
+DEFINE_STRING(arkanoid_score, "Zahl")
+DEFINE_STRING(arkanoid_game_over, "Spiel ist vorbei")
 
-DEFINE_STRING(tetris_next, "Next")
+DEFINE_STRING(tetris_next, "Folgend")
 DEFINE_STRING(tetris_level, "Level: ")
-DEFINE_STRING(tetris_score, "Score: ")
-DEFINE_STRING(tetris_game_over, "Game Over")
+DEFINE_STRING(tetris_score, "Zahl: ")
+DEFINE_STRING(tetris_game_over, "Spiel ist vorbei")
 
-DEFINE_STRING(snake_level, "level")
-DEFINE_STRING(snake_length, "length")
-DEFINE_STRING(snake_lives, "lifes")
-DEFINE_STRING(snake_score, "score")
-DEFINE_STRING(snake_game_over, "game over")
+DEFINE_STRING(snake_level, "Level")
+DEFINE_STRING(snake_length, "Länge")
+DEFINE_STRING(snake_lives, "Leben")
+DEFINE_STRING(snake_score, "Zahl")
+DEFINE_STRING(snake_game_over, "Spiel ist vorbei")
 
-DEFINE_STRING(pacman_level, "level")
-DEFINE_STRING(pacman_score, "score")
-DEFINE_STRING(pacman_ready, "ready!")
-DEFINE_STRING(pacman_game_over, "game over")
+DEFINE_STRING(pacman_level, "Level")
+DEFINE_STRING(pacman_score, "Zahl")
+DEFINE_STRING(pacman_ready, "Sei bereit!")
+DEFINE_STRING(pacman_game_over, "Spiel ist vorbei")
 
 #undef DEFINE_STRING
 
