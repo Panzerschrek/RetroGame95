b79e593326aa0fc6fcda713a039b274b81908c9c
Panzerschrek <AntiPanzerschrek@yandex.ru>
2022-10-04 09:48:52 +0300

    Risujem, kak-to, igrovoje pole.

--- src/GameBattleCity.cpp
+++ src/GameBattleCity.cpp
@@ -1,10 +1,19 @@
 #include "GameBattleCity.hpp"
 #include "GameMainMenu.hpp"
 #include "Draw.hpp"
+#include "SpriteBMP.hpp"
+#include "Sprites.hpp"
 
 GameBattleCity::GameBattleCity(SoundPlayer& sound_player)
 	: sound_player_(sound_player)
 {
+	for(uint32_t y = 0; y < c_field_height; ++y)
+	for(uint32_t x = 0; x < c_field_width ; ++x)
+	{
+		Block& block = field_[x + y * c_field_width];
+		block.type = BlockType((y + x) % 5);
+		block.destruction_mask = 0xF;
+	}
 }
 
 void GameBattleCity::Tick(const std::vector<SDL_Event>& events, const std::vector<bool>& keyboard_state)
@@ -24,7 +33,48 @@ void GameBattleCity::Draw(const FrameBuffer frame_buffer) const
 {
 	FillWholeFrameBuffer(frame_buffer, g_color_black);
 
-	DrawText(frame_buffer, g_color_white, 0, 0, "battle city");
+	const uint32_t field_width  = c_field_width  * c_block_size;
+	const uint32_t field_height = c_field_height * c_block_size;
+
+	const uint32_t field_offset_x = c_block_size * 2;
+	const uint32_t field_offset_y = (frame_buffer.height - field_height) / 2;
+
+	const uint32_t field_x_end = field_offset_x + field_width;
+	const uint32_t field_y_end = field_offset_y + field_height;
+
+	const Color32 border_color = g_cga_palette[8];
+	FillRect(frame_buffer, border_color, 0, 0, frame_buffer.width, field_offset_y);
+	FillRect(frame_buffer, border_color, 0, field_offset_y + field_height, frame_buffer.width, frame_buffer.height - field_y_end);
+	FillRect(frame_buffer, border_color, 0, field_offset_y, field_offset_x, field_height);
+	FillRect(frame_buffer, border_color, field_x_end, field_offset_y, frame_buffer.width - field_x_end, field_height);
+
+	// TODO - use proper sprites.
+	const SpriteBMP block_sprites[]
+	{
+		Sprites::tetris_block_small_4,
+		Sprites::tetris_block_small_7,
+		Sprites::tetris_block_small_5,
+		Sprites::tetris_block_small_1,
+		Sprites::tetris_block_small_2,
+		Sprites::tetris_block_small_6,
+	};
+
+	for(uint32_t y = 0; y < c_field_height; ++y)
+	for(uint32_t x = 0; x < c_field_width ; ++x)
+	{
+		const Block& block = field_[x + y * c_field_width];
+		if(block.type == BlockType::Empty || block.destruction_mask == 0)
+		{
+			continue;
+		}
+
+		DrawSpriteWithAlpha(
+			frame_buffer,
+			block_sprites[size_t(block.type)],
+			0,
+			field_offset_x + x * c_block_size,
+			field_offset_y + y * c_block_size);
+	}
 }
 
 GameInterfacePtr GameBattleCity::AskForNextGameTransition()
--- src/GameBattleCity.hpp
+++ src/GameBattleCity.hpp
@@ -15,6 +15,30 @@ public: // GameInterface
 	virtual GameInterfacePtr AskForNextGameTransition() override;
 
 private:
+	enum class BlockType
+	{
+		Empty,
+		Brick,
+		Concrete,
+		Trees,
+		Water,
+	};
+
+	struct Block
+	{
+		BlockType type : 4;
+		// 0 if totally destroyed.
+		uint8_t destruction_mask : 4;
+	};
+
+	static const constexpr uint32_t c_field_width  = 32;
+	static const constexpr uint32_t c_field_height = 26;
+
+	static const constexpr uint32_t c_block_size = 8; // In pixels.
+
+private:
 	SoundPlayer& sound_player_;
 	GameInterfacePtr next_game_;
+
+	Block field_[c_field_width * c_field_height];
 };
